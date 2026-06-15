fcf84842cc1255e9bfba77098bb765a5341ff7fc
Kojo Bailey <kojomolojo@gmail.com>
2026-06-15 02:45:59 +0100

    Add notifications to screenshots

--- globals.lua
+++ globals.lua
@@ -4,6 +4,7 @@ return {
 	terminal = "kitty",
 	projectsTerminal = 'kitty --working-directory ~/Projects zsh -c \'TS="+%Y %b %d %H:%M"; echo "\\e[1;31m[ Projects ]\\e[0m"; eza -l --icons --hyperlink --no-permissions --no-user --no-filesize --time-style=$TS --sort=modified --reverse; echo "\n\\e[3;37m// Inactive\\e[0m"; eza .inactive -la --icons --hyperlink --no-permissions --no-user --no-filesize --time-style=$TS --sort=modified --reverse; exec zsh\'',
 	fileManager = "dolphin",
-	screenshotAreaTool = "grim -g \"$(slurp)\" - | tee ~/Pictures/screenshots/$(date +%Y%m%d_%H:%M:%S).png | wl-copy",
-	screenshotScreenTool = "grim - | tee ~/Pictures/screenshots/$(date +%Y%m%d_%H:%M:%S).png | wl-copy",
+
+	screenshotAreaTool = "grim -g \"$(slurp)\" - | tee /tmp/latest_screenshot.png ~/Pictures/screenshots/$(date +%Y%m%d_%H:%M:%S).png | wl-copy && notify-send 'Screenshot taken' 'Saved to \"~/Pictures/screenshots\" and copied to clipboard.' -i /tmp/latest_screenshot.png -t 3000",
+	screenshotScreenTool = "grim - | tee /tmp/latest_screenshot.png ~/Pictures/screenshots/$(date +%Y%m%d_%H:%M:%S).png | wl-copy && notify-send 'Screenshot taken' 'Saved to \"~/Pictures/screenshots\" and copied to clipboard.' -i /tmp/latest_screenshot.png -t 3000",
 }
